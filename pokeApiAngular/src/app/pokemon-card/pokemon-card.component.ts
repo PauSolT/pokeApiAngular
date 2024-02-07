efc9e4d42b1b3a9cf4e250714af104815643974a
pau <pausoltor@gmail.com>
2024-02-07 13:44:42 +0100

    Display pokemon card info

--- pokeApiAngular/src/app/pokemon-card/pokemon-card.component.ts
+++ pokeApiAngular/src/app/pokemon-card/pokemon-card.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { PokemonData } from '../../interfaces/pokemon-data';
+import { PokedexService } from '../../services/pokedex.service';
+import { NgOptimizedImage, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-card',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, TitleCasePipe],
   templateUrl: './pokemon-card.component.html',
-  styleUrl: './pokemon-card.component.css'
+  styleUrl: './pokemon-card.component.css',
 })
 export class PokemonCardComponent {
+  randomIdPokemon: number[] = [];
+  randomPokemonData: PokemonData[] = [];
 
+  constructor(private pokedexService: PokedexService) {
+    for (let index = 0; index < 12; index++) {
+      let randomNumber = Math.floor(Math.random() * 1017) + 1;
+      while (this.randomIdPokemon.includes(randomNumber)) {
+        randomNumber = Math.floor(Math.random() * 1017) + 1;
+      }
+      this.randomIdPokemon = [...this.randomIdPokemon, randomNumber];
+      this.pokedexService
+        .getPokemonData(randomNumber.toString())
+        .then((pokemonData: PokemonData) => {
+          this.randomPokemonData = [...this.randomPokemonData, pokemonData];
+        });
+    }
+  }
+  
 }
